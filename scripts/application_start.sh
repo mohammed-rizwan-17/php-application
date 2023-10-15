5694517c3db535faa4fbeece45fc0d0eef8a6e40
mohammed-rizwan-17 <mohammedrizu17@gmail.com>
2023-10-16 01:33:04 +0530

    update the appspec

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,16 +2,17 @@
 #!/bin/bash
 
 # Define your ECR image and desired container name
-ECR_IMAGE="333164040417.dkr.ecr.us-east-1.amazonaws.com/my-php-app:latest"
+ECR_IMAGE="333164040417.dkr.ecr.ap-south-1.amazonaws.com/my-php-app:latest"
 CONTAINER_NAME="my-php-app"
 
+
 # Step 1: Authenticate with ECR (ensure you have AWS credentials configured)
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 333164040417.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 333164040417.dkr.ecr.ap-south-1.amazonaws.com
 # Step 2: Pull the ECR image
-docker pull 333164040417.dkr.ecr.us-east-1.amazonaws.com/my-php-app:latest
+docker pull 333164040417.dkr.ecr.ap-south-1.amazonaws.com/my-php-app:latest
 
 # Step 3: Build and run the Docker container
-sudo docker run -d --name $CONTAINER_NAME -p 8080:80 333164040417.dkr.ecr.us-east-1.amazonaws.com/my-php-app:latest
+sudo docker run -d --name $CONTAINER_NAME -p 8080:80 333164040417.dkr.ecr.ap-south-1.amazonaws.com/my-php-app:latest
 
 # Optionally, you can check if the container is running
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
